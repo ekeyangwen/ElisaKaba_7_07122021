ca81be6b9feb325ac57ddd28c29218120b025c52
Ekeyangwen <ekeyangwen@gmail.com>
2021-12-10 14:59:48 +0100

    Ajout de la boucle pour lire ingredients

--- src/indexPP.js
+++ src/indexPP.js
@@ -1,35 +1,39 @@
 import { recipe } from "./recettes.js";
 import { oneIngredient } from "./recettes.js";
 const url = "./src/Recipes.json";
+let newRecette;
 
 // récupération des données JSON
-const getData = async () =>
-  await fetch(url).then((response) => response.json());
+fetch(url)
+  .then((response) => response.json())
 
-// fonction pour créer les fiches recettes
-const recettes = (data) => {
-  for (let recette of data.recipes) {
-    console.log(data.recipes);
-    let singleRecette = new recipe(recette);
-    let main = document.getElementById("main");
-    main.innerHTML += singleRecette.createRecipe();
-  }
-};
+  // fonction pour créer les fiches recettes
 
+  .then((data) => {
+    for (let recette of data.recipes) {
+      newRecette = new recipe(recette);
+      let main = document.getElementById("main");
+      main.innerHTML += newRecette.createRecipe();
+    }
+
+    // console.log(data);
+    // console.log(data.recipes.length);
+    let i = 0;
+
+    for (let ingredient of data.recipes[i].ingredients) {
+      for (i; i < data.recipes.length; i++) {
+        let newIngredient = new oneIngredient(ingredient[i]);
+        let liste = document.querySelector(".ingredients");
+        liste.innerHTML += newIngredient.createIngredient();
+      }
+    }
+  });
 // fonction pour récupérer les ingrédients
-const ingredients = (data) => {
-  for (let ingredient of data.ingredients) {
-    let singleIngredient = new oneIngredient(ingredient);
-    let listes = document.querySelector(".listeIngredients");
-    listes.innerHTML += singleIngredient.createIngredient();
-  }
-};
-
-// fonction globale d'intialisation de toutes les fonctions
-async function init() {
-  const data = await getData();
-  recettes(data);
-  ingredients(data);
-}
-
-init();
+// .then((data) => {
+//   for (let ingredient in data.ingredients) {
+//     console.log(ingredient);
+//     let singleIngredient = new oneIngredient(ingredient);
+//     let liste = document.querySelector(".listeIngredients");
+//     liste.innerHTML += singleIngredient.createIngredient();
+//   }
+// });
--- src/recettes.js
+++ src/recettes.js
@@ -3,7 +3,6 @@ export class recipe {
     this.id = data.id;
     this.name = data.name;
     this.servings = data.servings;
-    this.ingredients = data.ingredients;
     this.time = data.time;
     this.description = data.description;
     this.appliance = data.appliance;
@@ -15,7 +14,7 @@ export class recipe {
     <section class="recette">
      <div id="recetteImg"></div>
      <div id="legendeRecette">
-      <section id="recetteDescription">
+      <section id="recetteAndTime">
         <div id="titreListe">
           <h1 id="titre">${this.name}</h1>
           </div>
@@ -26,24 +25,32 @@ export class recipe {
           </div>        
         </section>
       </section>
-          <div class="description">${this.description}</div>
-   
+      <section id= "recetteDesciption">
+        <div class="ingredients"></div>
+        <div class="description">${this.description}</div>
+          
+    </section>
     </section>
         `;
   }
 }
+
 export class oneIngredient {
-  constructor(data) {
-    this.ingredient = data.ingredient;
-    this.quantity = data.quantity;
-    this.unit = data.unit;
+  constructor(ingredients) {
+    this.ingredient = ingredients.ingredient;
+    this.quantity = ingredients.quantity;
+    this.unit = ingredients.unit;
   }
+  // export class oneIngredient {
+  //   constructor(data) {
+  //
+  //   }
 
   createIngredient() {
     return `
-<div class="ingredients">
-            <ul class="listeIngredients"> ${this.ingredient} ${this.quantity} ${this.unit}</ul> 
-          </div>         
-`;
+
+   <ul class="listeIngredients"> ${this.ingredient} ${this.quantity} ${this.unit}</ul>
+
+  `;
   }
 }
