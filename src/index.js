dfb4921beb33600dfbffb6366ee1ae2adc37a2f3
Ekeyangwen <ekeyangwen@gmail.com>
2021-12-24 10:35:24 +0100

    Création des datalist sur inputs

--- src/index.js
+++ src/index.js
@@ -23,29 +23,29 @@ const afficherRechercherRecettes = (recipes) => {
     value = input.value.toLowerCase();
     if (value.length >= 3) {
       recherche(value);
+      console.log(value);
     }
   }
-  // console.log(recipes.ingredient);
-  // const ingredientsMap = items.ingredients.map((ingredient) => {
-  //   console.log(ingredient);
-  //   return ingredient;
-  // });
+
   function recherche(value) {
     results = recipes.filter((items) => {
       items.name.toLowerCase().includes(value) ||
-        // console.log(items.name);
-        items.description.toLowerCase().includes(value) ||
-        items.ingredients.map((ingredient) => {
-          let ingredientMap = ingredient.ingredient;
-          let mapIncludes = ingredientMap.toLowerCase().includes(value);
-          return mapIncludes;
-        });
+        // console.log(items.name)
+        items.description.toLowerCase().includes(value);
+      // console.log(items.description);
+      // items.ingredients.map((ingredient) => {
+      //   console.log(ingredient);
+      //   let ingredientMap = ingredient.ingredient;
+      //   console.log(ingredientMap);
+      //   let mapIncludes = ingredientMap.toLowerCase().includes(value);
+      //   console.log(mapIncludes);
+      //   return mapIncludes;
+      // });
     });
     console.log(results);
     displayRecette(results);
   }
 };
-
 const generateFilters = (recipes) => {
   let ingredientsT = [];
   let applianceT = [];
@@ -78,6 +78,46 @@ const generateFilters = (recipes) => {
   const filteredUstensils = ustensilsT.filter(function (ele, pos) {
     return ustensilsT.indexOf(ele) == pos;
   });
+  // console.log(filteredUstensils);
+
+  let BtnIngredient = document.getElementById("triBtnIngredients");
+  BtnIngredient.addEventListener("click", AfficherInputIngredients);
+
+  function AfficherInputIngredients() {
+    const list = document.getElementById("inputIngredients");
+
+    filteredIngredients.forEach((item) => {
+      let option = document.createElement("option");
+      option.value = item;
+      list.appendChild(option);
+    });
+  }
+
+  let BtnAppareils = document.getElementById("triBtnAppareil");
+  BtnAppareils.addEventListener("click", AfficherInputAppareils);
+
+  function AfficherInputAppareils() {
+    const list = document.getElementById("inputAppareils");
+
+    filteredAppliance.forEach((item) => {
+      let option = document.createElement("option");
+      option.value = item;
+      list.appendChild(option);
+    });
+  }
+
+  let BtnUstensiles = document.getElementById("triBtnUstensiles");
+  BtnUstensiles.addEventListener("click", AfficherInputUstensiles);
+
+  function AfficherInputUstensiles() {
+    const list = document.getElementById("inputUstensiles");
+
+    filteredUstensils.forEach((item) => {
+      let option = document.createElement("option");
+      option.value = item;
+      list.appendChild(option);
+    });
+  }
 };
 
 // fonction globale d'intialisation de toutes les fonctions
